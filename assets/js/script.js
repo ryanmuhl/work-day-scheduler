95c10cade3e0238c94e50e29b3258520d65e2313
ryanmuhl <ryanmuhl@hotmail.com>
2022-03-06 17:25:12 -0500

    add color codded time intervals to rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
-//Display Current Date at Top of Screen
+//Display Current Date at Jumbotron
 var currentDateAndTime = (moment().format("MMMM D, YYYY"))
-    $("#currentDay").text(currentDateAndTime);
+$("#currentDay").text(currentDateAndTime);
 
-    
+var timeFlies = function () {
+    var currentTime = moment().hour()
+    console.log (currentTime)
+    for (var i = 9; i < 18; i++) {
+        var taskArea = $("#time" + i)
+        if (currentTime > i) {
+            $(taskArea).addClass("past");
+        } else if (currentTime === i) {
+            $(taskArea).addClass("present");
+        } else {
+            $(taskArea).addClass("future")
+        }
+    }
+}
+
+setInterval(function(){
+    timeFlies();},1000*60*60);
+
+
+timeFlies ()
